6a95d779fb57124330aaa456d5fbf6d748011d1e
ryaen mateu <ryaenmateu22@gamil.com>
2020-08-29 13:59:47 +0200

    finale2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 3,  
         slidesToScroll: 3,
         infinite: true,
         dots: true
